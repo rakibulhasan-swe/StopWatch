19188807c4d1cb8ac73e5350e57b26baec9473b8
unknown <rakibulhasan.seu.cse@gmail.com>
2022-09-25 15:33:54 +0600

    done

--- script.js
+++ script.js
@@ -9,42 +9,41 @@ window.onload = function (){
     let Interval;
 
     buttonStart.onclick = function () {
-         clearInterval(Interval);
-         Interval =setInterval(startTimer, 10);
+        clearInterval(Interval);
+        Interval =setInterval(startTimer, 10);
     };
 
     buttonStop.onclick =function () {
-         clearInterval(Interval);
+        clearInterval(Interval);
     };
 
     buttonReset.onclick =function () {
-         clearInterval(Interval);
-         tens ="00";
-         seconds = "00";
-         appendTens.innerHTML = tens;
-         appendSeconds.innerHTML =seconds;
+        clearInterval(Interval);
+        tens ="00";
+        seconds = "00";
+        appendTens.innerHTML = tens;
+        appendSeconds.innerHTML =seconds;
     };
 
     function startTimer () {
-         tens++;
-         if(tens <= 9){
+        tens++;
+        if(tens <= 9){
             appendTens.innerHTML ="0" + tens; 
-         }
+        }
 
-         if(tens > 9) {
+        if(tens > 9){
             appendTens.innerHTML = tens;
-         }
+        }
 
-         if(tens > 99) {
-              console.log("seconds");
-              seconds++;
-              appendSeconds.innerHTML = "0" + seconds;
-              tens = 0;
-              appendTens.innerHTML = "0" + 0;
-         }
+        if(tens > 99){
+            seconds++;
+            appendSeconds.innerHTML = "0" + seconds;
+            tens = 0;
+            appendTens.innerHTML = "0" + 0;
+        }
 
-         if(seconds > 9) {
-              appendSeconds.innerHTML = seconds;
-         }
+        if(seconds > 9){
+            appendSeconds.innerHTML = seconds;
+        }
     }
 };
